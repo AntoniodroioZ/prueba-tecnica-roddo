f89f738ff8f66b2e06ee362c90f56d63ddb33192
Antonio <antonybey13@gmail.com>
2023-01-15 22:36:24 -0600

    solucion de bugs

--- Prueba_tecnica/index.js
+++ Prueba_tecnica/index.js
@@ -58,259 +58,156 @@ app.get('/api/getAll', async (req, res) => {
     res.send(data);
 });
 
-// app.post('/api/createResource', async (req, res) => {
-//     let bandera = true;
-//     let newResource = new Object();
-//     const user = req.query.user;
-//     const hashToken = req.query.hash;
-
-//     if(req.query.description.length == 0 || req.query.address.length == 0 || req.query.contactPhone.length == 0 || req.query.contactMail.length === 0){
-//         bandera = false;
-//     } else{
-//         newResource.description = req.query.description;
-//         newResource.address = req.query.address;
-//         newResource.contactPhone = req.query.contactPhone;
-//         newResource.contactMail = req.query.contactMail;
-//     }
-//     if(req.query.field == "" || typeof parseInt(req.query.field) != "number" || Number.isNaN(parseInt(req.query.field))==true){
-//         bandera = false;
-
-//     }else{
-//         newResource.field = req.query.field;
-//     }
-//     if(req.query.construction == "" || typeof parseInt(req.query.construction) != "number" || Number.isNaN(parseInt(req.query.construction))==true){
-//         bandera = false;
-
-//     }else{
-//         newResource.construction = req.query.construction;
-//     }
-//     if(req.query.bathrooms == "" || typeof parseInt(req.query.bathrooms) != "number" || Number.isNaN(parseInt(req.query.bathrooms))==true){
-//         bandera = false;
-
-//     }else{
-//         newResource.bathrooms = req.query.bathrooms;
-//     }
-//     if(req.query.bedrooms == "" || typeof parseInt(req.query.bedrooms) != "number" || Number.isNaN(parseInt(req.query.bedrooms))==true){
-//         bandera = false;
-
-//     }else{
-//         newResource.bedrooms = req.query.bedrooms;
-//     }
-//     if(req.query.parkingLots == "" || typeof parseInt(req.query.parkingLots) != "number" || Number.isNaN(parseInt(req.query.parkingLots))==true){
-//         bandera = false;
-
-//     }else{
-//         newResource.parkingLots = req.query.parkingLots;
-//     }
-
-//     if(bandera == false){
-//         res.send({
-//             "message": "El recurso no se pudo agregar correctamente, intente de nuevo",
-//             "code":1
-//         });
-//     }else{
-//         const data = await createResource(newResource,bandera,user,hashToken);
-//         if (data == true && bandera == true) {
-//             res.send({
-//                 "message": "Recurso agregado correctamente",
-//                 "code":0
-//             });
-//         }else if(data == 0){
-//             res.send({
-//                 "message": "No tienes autorización para hacer esta acción",
-//                 "code":2
-//             });
-//         }
-//     }
-// });
 app.post('/api/createResource', async (req, res) => {
     let bandera = true;
     let newResource = new Object();
     const user = req.query.user;
     const hashToken = req.query.hash;
 
-
-    let description = req.query.description;
-    let address = req.query.address;
-    let contactPhone = req.query.contactPhone;
-    let contactMail = req.query.contactMail;
-    let field = req.query.field;
-    let construction = req.query.construction;
-    let bathrooms = req.query.bathrooms;
-    let bedrooms = req.query.bedrooms;
-    let parkingLots = req.query.parkingLots;
-
-
-    // if (bandera == false) {
-    //     res.send({
-    //         "message": "El recurso no se pudo agregar correctamente, intente de nuevo",
-    //         "code": 1
-    //     });
-    // } else {
-    //     const data = await createResource(newResource, bandera, user, hashToken);
-    //     if (data == true && bandera == true) {
-    //         res.send({
-    //             "message": "Recurso agregado correctamente",
-    //             "code": 0
-    //         });
-    //     } else if (data == 0) {
-    //         res.send({
-    //             "message": "No tienes autorización para hacer esta acción",
-    //             "code": 2
-    //         });
-    //     }
-    // }
-
-
-    const data = await createResource(bandera, user, hashToken, description,address,contactPhone,contactMail,field,construction,bathrooms,bedrooms,parkingLots);
-    if (data == true) {
-        res.send({
-            "message": "Recurso agregado correctamente",
-            "code": 0
-        });
-    } else {
-        res.send({
-            "message": "No tienes autorización para hacer esta acción",
-            "code": 2
-        });
+    if(req.query.description.length == 0 || req.query.address.length == 0 || req.query.contactPhone.length == 0 || req.query.contactMail.length === 0){
+        bandera = false;
+    } else{
+        newResource.description = req.query.description;
+        newResource.address = req.query.address;
+        newResource.contactPhone = req.query.contactPhone;
+        newResource.contactMail = req.query.contactMail;
     }
+    if(req.query.field == "" || typeof parseInt(req.query.field) != "number" || Number.isNaN(parseInt(req.query.field))==true){
+        bandera = false;
+        
+    }else{
+        newResource.field = req.query.field;
+    }
+    if(req.query.construction == "" || typeof parseInt(req.query.construction) != "number" || Number.isNaN(parseInt(req.query.construction))==true){
+        bandera = false;
+        
+    }else{
+        newResource.construction = req.query.construction;
+    }
+    if(req.query.bathrooms == "" || typeof parseInt(req.query.bathrooms) != "number" || Number.isNaN(parseInt(req.query.bathrooms))==true){
+        bandera = false;
+        
+    }else{
+        newResource.bathrooms = req.query.bathrooms;
+    }
+    if(req.query.bedrooms == "" || typeof parseInt(req.query.bedrooms) != "number" || Number.isNaN(parseInt(req.query.bedrooms))==true){
+        bandera = false;
+        
+    }else{
+        newResource.bedrooms = req.query.bedrooms;
+    }
+    if(req.query.parkingLots == "" || typeof parseInt(req.query.parkingLots) != "number" || Number.isNaN(parseInt(req.query.parkingLots))==true){
+        bandera = false;
+        
+    }else{
+        newResource.parkingLots = req.query.parkingLots;
+    }
+
+    if(bandera == false){
+        res.send({
+            "message": "El recurso no se pudo agregar correctamente, intente de nuevo",
+            "code":1
+        });
+    }else{
+        const data = await createResource(newResource,bandera,user,hashToken);
+        if (data == true && bandera == true) {
+            res.send({
+                "message": "Recurso agregado correctamente",
+                "code":0
+            });
+        }else if(data == 0){
+            res.send({
+                "message": "No tienes autorización para hacer esta acción",
+                "code":2
+            });
+        }
+    }
+
 
 });
 
-app.post('/api/deleteResource', async (req, res) => {
+app.post('/api/deleteResource', async (req,res)=>{
     const id = req.query.id;
     const user = req.query.user;
     const hashToken = req.query.hash;
-    const data = await deleteResource(id, user, hashToken);
-
-    if (data) {
+    const data = await deleteResource(id,user,hashToken);
+    
+    if(data){
         res.send({
             "message": "Recurso eliminado correctamente",
-            "code": 0
+            "code":0
         });
-    } else {
+    } else{
         res.send({
             "message": "No tienes autorización para hacer esta acción",
-            "code": 2
+            "code":2
         });
     }
 })
 
-// app.post('/api/updateResource', async (req,res)=>{
-//     const id = req.query.id;
-//     const user = req.query.user;
-//     const hashToken = req.query.hash;
-//     let bandera = true;
-
-//     let editResource = new Object();
-
-//     if(req.query.description.length == 0 || req.query.address.length == 0 || req.query.contactPhone.length == 0 || req.query.contactMail.length === 0){
-//         bandera = false;
-//     } else{
-//         editResource.description = req.query.description;
-//         editResource.address = req.query.address;
-//         editResource.contactPhone = req.query.contactPhone;
-//         editResource.contactMail = req.query.contactMail;
-//     }
-//     if(req.query.field == "" || typeof parseInt(req.query.field) != "number" || Number.isNaN(parseInt(req.query.field))==true){
-//         bandera = false;
-//     }else{
-//         editResource.field = req.query.field;
-//     }
-//     if(req.query.construction == "" || typeof parseInt(req.query.construction) != "number" || Number.isNaN(parseInt(req.query.construction))==true){
-//         bandera = false;
-//     }else{
-//         editResource.construction = req.query.construction;
-//     }
-//     if(req.query.bathrooms == "" || typeof parseInt(req.query.bathrooms) != "number" || Number.isNaN(parseInt(req.query.bathrooms))==true){
-//         bandera = false;
-//     }else{
-//         editResource.bathrooms = req.query.bathrooms;
-//     }
-//     if(req.query.bedrooms == "" || typeof parseInt(req.query.bedrooms) != "number" || Number.isNaN(parseInt(req.query.bedrooms))==true){
-//         bandera = false;
-//     }else{
-//         editResource.bedrooms = req.query.bedrooms;
-//     }
-//     if(req.query.parkingLots == "" || typeof parseInt(req.query.parkingLots) != "number" || Number.isNaN(parseInt(req.query.parkingLots))==true){
-//         bandera = false;
-//     }else{
-//         editResource.parkingLots = req.query.parkingLots;
-//     }
-
-//     if(bandera == false){
-//         res.send({
-//             "message": "El recurso no se pudo editar correctamente, intente de nuevo",
-//             "code":1
-//         });
-//     }else{
-//         const data = await updateResource(editResource,id,bandera,user,hashToken);
-//         if (data == true && bandera == true) {
-//             res.send({
-//                 "message": "Recurso editado correctamente",
-//                 "code":0
-//             });
-//         }else if(data == 0){
-//             res.send({
-//                 "message": "No tienes autorización para hacer esta acción",
-//                 "code":2
-//             });
-//         }
-//     }
-
-// })
-
-app.post('/api/updateResource', async (req, res) => {
+app.post('/api/updateResource', async (req,res)=>{
     const id = req.query.id;
     const user = req.query.user;
     const hashToken = req.query.hash;
     let bandera = true;
 
-    let description = req.query.description;
-    let address = req.query.address;
-    let contactPhone = req.query.contactPhone;
-    let contactMail = req.query.contactMail;
-    let field = req.query.field;
-    let construction = req.query.construction;
-    let bathrooms = req.query.bathrooms;
-    let bedrooms = req.query.bedrooms;
-    let parkingLots = req.query.parkingLots;
+    let editResource = new Object();
 
-    // if(bandera == false){
-    //     res.send({
-    //         "message": "El recurso no se pudo editar correctamente, intente de nuevo",
-    //         "code":1
-    //     });
-    // }else{
-    //     const data = await updateResource(editResource,id,bandera,user,hashToken);
-    //     if (data == true && bandera == true) {
-    //         res.send({
-    //             "message": "Recurso editado correctamente",
-    //             "code":0
-    //         });
-    //     }else if(data == 0){
-    //         res.send({
-    //             "message": "No tienes autorización para hacer esta acción",
-    //             "code":2
-    //         });
-    //     }
-    // }
-
-    const data = await updateResource( id, bandera, user, hashToken, description, address, contactPhone, contactMail, field, construction, bathrooms, bedrooms,parkingLots);
-    if (data == true) {
-        res.send({
-            "message": "Recurso editado correctamente",
-            "code": 0
-        });
-    } else {
-        res.send({
-            "message": "No tienes autorización para hacer esta acción",
-            "code": 2
-        });
+    if(req.query.description.length == 0 || req.query.address.length == 0 || req.query.contactPhone.length == 0 || req.query.contactMail.length === 0){
+        bandera = false;
+    } else{
+        editResource.description = req.query.description;
+        editResource.address = req.query.address;
+        editResource.contactPhone = req.query.contactPhone;
+        editResource.contactMail = req.query.contactMail;
+    }
+    if(req.query.field == "" || typeof parseInt(req.query.field) != "number" || Number.isNaN(parseInt(req.query.field))==true){
+        bandera = false;
+    }else{
+        editResource.field = req.query.field;
+    }
+    if(req.query.construction == "" || typeof parseInt(req.query.construction) != "number" || Number.isNaN(parseInt(req.query.construction))==true){
+        bandera = false;
+    }else{
+        editResource.construction = req.query.construction;
+    }
+    if(req.query.bathrooms == "" || typeof parseInt(req.query.bathrooms) != "number" || Number.isNaN(parseInt(req.query.bathrooms))==true){
+        bandera = false;
+    }else{
+        editResource.bathrooms = req.query.bathrooms;
+    }
+    if(req.query.bedrooms == "" || typeof parseInt(req.query.bedrooms) != "number" || Number.isNaN(parseInt(req.query.bedrooms))==true){
+        bandera = false;
+    }else{
+        editResource.bedrooms = req.query.bedrooms;
+    }
+    if(req.query.parkingLots == "" || typeof parseInt(req.query.parkingLots) != "number" || Number.isNaN(parseInt(req.query.parkingLots))==true){
+        bandera = false;
+    }else{
+        editResource.parkingLots = req.query.parkingLots;
     }
 
+    if(bandera == false){
+        res.send({
+            "message": "El recurso no se pudo editar correctamente, intente de nuevo",
+            "code":1
+        });
+    }else{
+        const data = await updateResource(editResource,id,bandera,user,hashToken);
+        if (data == true && bandera == true) {
+            res.send({
+                "message": "Recurso editado correctamente",
+                "code":0
+            });
+        }else if(data == 0){
+            res.send({
+                "message": "No tienes autorización para hacer esta acción",
+                "code":2
+            });
+        }
+    }
+    
 })
-
 app.get("/isPalindromo", (req, res) => {
     let palabra = req.query.palabra
     let cadena = palabra.toLowerCase();
